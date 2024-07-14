24b07976f664b1126321d93106e66039b3bace1c
fereidoon27 <mohammadamin.fereidoon@gmail.com>
2024-07-14 14:34:31 +0330

    multi_gpu0

--- bash_ex.sh
+++ bash_ex.sh
@@ -9,7 +9,8 @@ python parjax_cla_multi_GoogleColab.py \
   --output_dir=/content/jaxalpha/multi/out_multi_Z_A/1_5 \
   --parameter_path=/content/ParallelFold/params \
   --feature_files=/content/jaxalpha/multi/2LHC/feature.pkl,/content/jaxalpha/multi/2LHG/feature.pkl,/content/jaxalpha/multi/2LHE/feature.pkl,/content/jaxalpha/multi/2LHD/feature.pkl,/content/jaxalpha/multi/6UF2/feature.pkl \
-  --use_gpu_relax=true
+  --use_gpu_relax=true \
+  --benchmark=true
 
 
 python parjax_cla_multi_GoogleColab.py \
@@ -17,7 +18,8 @@ python parjax_cla_multi_GoogleColab.py \
   --output_dir=/content/jaxalpha/multi/out_multi_Z_A/6_10 \
   --parameter_path=/content/ParallelFold/params \
   --feature_files=/content/jaxalpha/multi/7CN6/feature.pkl,/content/jaxalpha/multi/7JTL/feature.pkl,/content/jaxalpha/multi/7CWP/feature.pkl,/content/jaxalpha/multi/6Y4F/feature.pkl,/content/jaxalpha/multi/6ZYC/feature.pkl \
-  --use_gpu_relax=true
+  --use_gpu_relax=true \
+  --benchmark=true
 
 
 
@@ -26,7 +28,8 @@ python parjax_cla_multi_GoogleColab.py \
   --output_dir=/content/jaxalpha/multi/out_multi_Z_A/11_15 \
   --parameter_path=/content/ParallelFold/params \
   --feature_files=/content/jaxalpha/multi/8EM5/feature.pkl,/content/jaxalpha/multi/8PBV/feature.pkl,/content/jaxalpha/multi/7ROA/feature.pkl,/content/jaxalpha/multi/7PZT/feature.pkl,/content/jaxalpha/multi/8D27/feature.pkl \
-  --use_gpu_relax=true
+  --use_gpu_relax=true \
+  --benchmark=true
   
   
 ####-------------------------------------------------------------------------------------------
@@ -38,16 +41,16 @@ python parjax_cla_multi_GoogleColab.py \
   --output_dir=/content/jaxalpha/multi/out_multi_A_Z/1_5 \
   --parameter_path=/content/ParallelFold/params \
   --feature_files=/content/jaxalpha/multi/6UF2/feature.pkl,/content/jaxalpha/multi/2LHG/feature.pkl,/content/jaxalpha/multi/2LHE/feature.pkl,/content/jaxalpha/multi/2LHD/feature.pkl,/content/jaxalpha/multi/2LHC/feature.pkl \
-  --use_gpu_relax=true
-
+  --use_gpu_relax=true \
+  --benchmark=true
 
 python parjax_cla_multi_GoogleColab.py \
   --fasta_paths=/content/jaxalpha/multi/6ZYC/6ZYC.fasta,/content/jaxalpha/multi/6Y4F/6Y4F.fasta,/content/jaxalpha/multi/7CWP/7CWP.fasta,/content/jaxalpha/multi/7JTL/7JTL.fasta,/content/jaxalpha/multi/7CN6/7CN6.fasta  \
   --output_dir=/content/jaxalpha/multi/out_multi_A_Z/6_10 \
   --parameter_path=/content/ParallelFold/params \
   --feature_files=/content/jaxalpha/multi/6ZYC/feature.pkl,/content/jaxalpha/multi/6Y4F/feature.pkl,/content/jaxalpha/multi/7CWP/feature.pkl,/content/jaxalpha/multi/7JTL/feature.pkl,/content/jaxalpha/multi/7CN6/feature.pkl \
-  --use_gpu_relax=true
-
+  --use_gpu_relax=true \
+  --benchmark=true
 
 
 python parjax_cla_multi_GoogleColab.py \
@@ -55,8 +58,8 @@ python parjax_cla_multi_GoogleColab.py \
   --output_dir=/content/jaxalpha/multi/out_multi_A_Z/11_15 \
   --parameter_path=/content/ParallelFold/params \
   --feature_files=/content/jaxalpha/multi/8D27/feature.pkl,/content/jaxalpha/multi/7PZT/feature.pkl,/content/jaxalpha/multi/7ROA/feature.pkl,/content/jaxalpha/multi/8PBV/feature.pkl,/content/jaxalpha/multi/8EM5/feature.pkl \
-  --use_gpu_relax=true
-
+  --use_gpu_relax=true \
+  --benchmark=true
 
 ####-------------------------------------------------------------------------------------------
 multi_same GoogleColab 
@@ -187,7 +190,7 @@ python parjax_cla_multi_gpu0.py \
 multi_Z_A benchmark
 claude-af 123
 
-python parjax_cla_multi.py \
+python parjax_cla_multi_gpu0.py \
   --fasta_paths=/home/koohi/fereidoon/ParallelFold/multi/2LHC/2LHC.fasta,/home/koohi/fereidoon/ParallelFold/multi/2LHG/2LHG.fasta,/home/koohi/fereidoon/ParallelFold/multi/2LHE/2LHE.fasta,/home/koohi/fereidoon/ParallelFold/multi/2LHD/2LHD.fasta,/home/koohi/fereidoon/ParallelFold/multi/6UF2/6UF2.fasta  \
   --output_dir=/home/koohi/fereidoon/ParallelFold/multi/out_multi_Z_A/benchmark/1_5 \
   --parameter_path=/home/koohi/fereidoon/ParallelFold/alldata/params \
@@ -196,7 +199,7 @@ python parjax_cla_multi.py \
   --benchmark=true
 
 
-python parjax_cla_multi_gpu0.py \
+python parjax_cla_multi.py \
   --fasta_paths=/home/koohi/fereidoon/ParallelFold/multi/7CN6/7CN6.fasta,/home/koohi/fereidoon/ParallelFold/multi/7JTL/7JTL.fasta,/home/koohi/fereidoon/ParallelFold/multi/7CWP/7CWP.fasta,/home/koohi/fereidoon/ParallelFold/multi/6Y4F/6Y4F.fasta,/home/koohi/fereidoon/ParallelFold/multi/6ZYC/6ZYC.fasta  \
   --output_dir=/home/koohi/fereidoon/ParallelFold/multi/out_multi_Z_A/benchmark/6_10 \
   --parameter_path=/home/koohi/fereidoon/ParallelFold/alldata/params \
